40ba637c69f83fc6e92f9bc8076502f3dde576bc
Hedy <hedywoo@gmail.com>
2016-05-23 19:25:30 -0600

    Add name field to users, all works

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160524004933) do
+ActiveRecord::Schema.define(version: 20160524012347) do
 
   create_table "users", force: :cascade do |t|
     t.string   "screen_name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20160524004933) do
     t.string   "oauth_token_secret"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+    t.string   "name"
   end
 
 end
