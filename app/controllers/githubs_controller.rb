0d53ccdbe79c19374c2ec6c03b9c7e3766b38445
Hedy <hedywoo@gmail.com>
2016-05-26 17:13:52 -0600

    No major changes

--- app/controllers/githubs_controller.rb
+++ app/controllers/githubs_controller.rb
@@ -2,4 +2,8 @@ class GithubsController < ApplicationController
   def followers
     @followers = GithubUser.new(current_user)
   end
+
+  def following
+    @following = GithubUser.new(current_user)
+  end
 end
